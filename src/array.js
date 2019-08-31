75e703a38ba64c8478114b16ad200703c377fec9
fanzhang <scofieldfan@163.com>
2019-08-31 09:05:25 +0800

    更新array

--- src/array.js
+++ src/array.js
@@ -1,45 +1,90 @@
-const array = {
-
-    flush(num = []) {
-        for (let i = 0; i < num.length; i++) {
-            let index = Math.floor(Math.random() * (num.length - 1));
-            let temp = num[i];
-            num[i] = num[index];
-            num[index] = temp;
+const flatten = function(ary) {
+    let result = [];
+    if (Object.prototype.toString.call(ary).slice(8, -1) === "Array") {
+        ary.forEach(item => {
+            if (Object.prototype.toString.call(item).slice(8, -1) === "Array") {
+                result = result.concat(flatten(item));
+            } else {
+                result.push(item);
+            }
+        });
+    } else {
+        result.push(ary);
+    }
+    return result;
+};
+const flush = function(num = []) {
+    for (let i = 0; i < num.length; i++) {
+        let index = Math.floor(Math.random() * (num.length - 1));
+        let temp = num[i];
+        num[i] = num[index];
+        num[index] = temp;
+    }
+};
+const binarySearch = function(ary, target) {
+    if (!Array.isArray(ary)) {
+        throw new TypeError("arg1 is not a array");
+    }
+    let start = 0,
+        end = ary.length - 1;
+    while (start <= end) {
+        let mid = Math.floor(start + (end - start) / 2);
+        if (ary[mid] === target) {
+            return mid;
+        } else if (ary[mid] < target) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
         }
-    },
-    threeSum(nums = [], target) {
-        if (nums.length === 0) {
-            return;
-        }
-        nums.sort((a, b) => a - b);
-
-        for (let j = 1; j <= nums.length - 2; j++) {
-            let i = 0, k = nums.length - 1;
-            let min = nums[i] + nums[j] + nums[j + 1];
-            let max = nums[j - 1] + nums[j] + nums[k];
-            if (target >= min && target <= max) {
-
-                while (i < j && j < k) {
-                    let curr = nums[i] + nums[j] + nums[k];
-                    if (curr === target) {
-
-                        let ret = [nums[i], nums[j], nums[k]];
-                        console.log("ret nums:", ret);
-                        return ret;
-                    } else if (curr < target) {
-                        i++;
-                    } else if (curr > target) {
-                        k--;
-                    }
-                }
-
+    }
+    return -1;
+};
+const quickSort = function(ary = [], start = 0, end = ary.length - 1) {
+    if (!Array.isArray(ary)) {
+        throw new TypeError("arg1 is not a array");
+    }
+    if (start >= end) {
+        return;
+    }
+    let index = partition(ary, start, end);
+    quickSort(ary, start, index - 1);
+    quickSort(ary, index + 1, end);
+    function partition(pAry, left, right) {
+        let priviot = pAry[right];
+        let start = left - 1;
+        for (let i = left; i <= right - 1; i++) {
+            if (pAry[i] < priviot) {
+                swap(pAry, ++start, i);
             }
         }
-        return [];
-
+        swap(pAry, ++start, right);
+        return start;
     }
-}
-export {
-    array
+    function swap(sAry, i, j) {
+        let temp = sAry[i];
+        sAry[i] = sAry[j];
+        sAry[j] = temp;
+    }
 };
+const twoSum = function(arys, target) {
+    if (!Array.isArray(arys)) {
+        throw new TypeError("arg1 is not a array");
+    }
+    const map = new Map();
+    for (let i = 0; i < arys.length; i++) {
+        let num = target - arys[i];
+        if (map.get(num) !== undefined) {
+            return [map.get(num), i];
+        }
+        map.set(arys[i], i);
+    }
+    return [];
+};
+const array = {
+    flatten,
+    binarySearch,
+    flush,
+    quickSort,
+    twoSum
+};
+export { array };
--- test/array.js
+++ test/array.js
@@ -1,11 +1,60 @@
-import { array } from '../lib/turtle.js';
-import test from 'ava';
+import { array } from "../lib/turtle.js";
+import test from "ava";
 
-test('array', t => {
+test("binarysearch", t => {
+    let ary = [0, 1, 5, 6, 10, 12, 13, 14, 15, 16, 20, 22, 24, 25];
+    t.is(array.binarySearch(ary, 2), -1);
+    t.is(array.binarySearch(ary, 0), 0);
+    t.is(array.binarySearch(ary, 25), ary.length - 1);
+    t.is(array.binarySearch(ary, 13), 6);
+    const error = t.throws(() => {
+        array.binarySearch(null, 13);
+    }, TypeError);
+    t.is(error.message, "arg1 is not a array");
+});
 
+test("flattern1", t => {
+    let arys = [1, 2, [3]];
+    let result = array.flatten(arys);
+    t.deepEqual(result, [1, 2, 3]);
+});
 
-    let ary = [2, 3, 1, -1, 0, 4];
-    t.deepEqual(array.threeSum(ary, 0), [-1, 0, 1]);
-    t.deepEqual(array.threeSum(ary, 5), [0, 1, 4]);
-    t.deepEqual(array.threeSum(ary, 9), [2, 3, 4]);
+test("flattern2", t => {
+    let arys = [1, 2, [3], [5, [6, 7]]];
+    let result = array.flatten(arys);
+    t.deepEqual(result, [1, 2, 3, 5, 6, 7]);
+});
+
+test("flattern3", t => {
+    let arys = [1, 2, [3], [5, [6, 7, [8, 9, 10]]]];
+    let result = array.flatten(arys);
+    t.deepEqual(result, [1, 2, 3, 5, 6, 7, 8, 9, 10]);
+});
+
+test("qsort1", t => {
+    let arys = [2, 1, 3];
+    array.quickSort(arys);
+    t.deepEqual(arys, [1, 2, 3]);
+});
+test("qsort2", t => {
+    let arys = [2, 1, 3, -1];
+    array.quickSort(arys);
+    t.deepEqual(arys, [-1, 1, 2, 3]);
+});
+test("qsort3", t => {
+    let arys = [2, 1, 3, -1, -1, -1];
+    array.quickSort(arys);
+    t.deepEqual(arys, [-1, -1, -1, 1, 2, 3]);
+});
+test("twosum", t => {
+    let arys = [2, 1, 3, -1, -1, -1];
+    t.deepEqual(array.twoSum(arys, 5), [0, 2]);
+});
+test("twosum1", t => {
+    let arys = [2, 1, 3, -1, -1, -1];
+    t.deepEqual(array.twoSum(arys, 3), [0, 1]);
+});
+test("twosum2", t => {
+    let arys = [2, 1, 3, -1, -1, -1];
+    t.deepEqual(array.twoSum(arys, 100), []);
 });
