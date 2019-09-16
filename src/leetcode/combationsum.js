b42e142b22db51e83ce7e3b92748637d66e57f9d
fanzhang <scofieldfan@163.com>
2019-09-16 08:48:03 +0800

    提交commbation

---
+++ src/leetcode/combationsum.js
@@ -0,0 +1,20 @@
+var combinationSum3 = function(k, n) {
+    let result = [];
+    function helper(start, k, n, ret) {
+        if (k === 0) {
+            if (n === 0) {
+                result.push(ret);
+            }
+            return;
+        }
+        for (let i = start; i < 10; i++) {
+            if (n - i < 0) {
+                break;
+            }
+            let temp = ret.concat(i);
+            helper(i + 1, k - 1, n - i, temp);
+        }
+    }
+    helper(1, k, n, []);
+    return result;
+};
